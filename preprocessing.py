40367c00e6636bb367a52e8e003b0b9be24f822d
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-05-04 08:31:12 +0800

    update

--- preprocessing.py
+++ preprocessing.py
@@ -5,65 +5,49 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
 
 
 class Rescale(object):
-    """Rescale the image in a sample to a given size.
-
-    Args:
-        output_size (tuple or int): Desired output size. If tuple, output is
-            matched to output_size. If int, smaller of image edges is matched
-            to output_size keeping aspect ratio the same.
-    """
-
     def __init__(self, output_size):
         assert isinstance(output_size, (int, tuple))
-        self.output_size = output_size
+        self._output_size = output_size
 
-    def __call__(self, sample):
-        image, landmarks = sample['image'], sample['landmarks']
+    def __call__(self, img, label):
+        image, landmarks = img, label
 
-        h, w = image.shape[:2]
-        if isinstance(self.output_size, int):
+        h, w = img.shape[:2]
+        if isinstance(self._output_size, int):
             if h > w:
-                new_h, new_w = self.output_size * h / w, self.output_size
+                new_h, new_w = self._output_size * h / w, self._output_size
             else:
-                new_h, new_w = self.output_size, self.output_size * w / h
+                new_h, new_w = self._output_size, self._output_size * w / h
         else:
-            new_h, new_w = self.output_size
+            new_h, new_w = self._output_size
 
         new_h, new_w = int(new_h), int(new_w)
+        img_resize = transforms.resize(img, (new_h, new_w))
 
-        img = transform.resize(image, (new_h, new_w))
 
-        # h and w are swapped for landmarks because for images,
-        # x and y axes are axis 1 and 0 respectively
         landmarks = landmarks * [new_w / w, new_h / h]
 
-        return {'image': img, 'landmarks': landmarks}
+        return img_resize, label
 
 
 class RandomCrop(object):
-    """Crop randomly the image in a sample.
-
-    Args:
-        output_size (tuple or int): Desired output size. If int, square crop
-            is made.
-    """
-
     def __init__(self, output_size):
         assert isinstance(output_size, (int, tuple))
         if isinstance(output_size, int):
-            self.output_size = (output_size, output_size)
+            self._output_size = (output_size, output_size)
         else:
             assert len(output_size) == 2
-            self.output_size = output_size
+            self._output_size = output_size
 
     def __call__(self, sample):
         image, landmarks = sample['image'], sample['landmarks']
 
         h, w = image.shape[:2]
-        new_h, new_w = self.output_size
+        new_h, new_w = self._output_size
 
         top = np.random.randint(0, h - new_h)
         left = np.random.randint(0, w - new_w)
