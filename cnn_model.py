4b37fd1949d6da0db6e8319ad80e1f16fd36b603
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-05-04 10:49:35 +0800

    update

---
+++ cnn_model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorboardX
+import pdb
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 53 * 53, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        bs = x.shape[0]
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(bs, -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 import torchvision
 
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorboardX
+import pdb
+
+from cnn_model import CNN
+
+NUM_EPOCH = 2
+USE_CUDA = False
+PRINT_EVERY = 10
+DEVICE = torch.device("cuda") if (torch.cuda.is_available()
+                                  and USE_CUDA) else torch.device("cpu")
 
 data_transform = torchvision.transforms.Compose([
     torchvision.transforms.RandomSizedCrop(224),
@@ -14,5 +24,39 @@ data_transform = torchvision.transforms.Compose([
     torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 ])
-dataset = torchvision.datasets.ImageFolder(root='./animal-10/train/',
-                                           transform=data_transform)
+train_ds = torchvision.datasets.ImageFolder(root='./animal-10/train/',
+                                            transform=data_transform)
+val_ds = torchvision.datasets.ImageFolder(root='./animal-10/val/',
+                                          transform=data_transform)
+
+train_dl = torch.utils.data.DataLoader(train_ds,
+                                       batch_size=50,
+                                       shuffle=True,
+                                       num_workers=4)
+val_dl = torch.utils.data.DataLoader(val_ds,
+                                     batch_size=50,
+                                     shuffle=True,
+                                     num_workers=4)
+
+model = CNN().to(DEVICE)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+for epoch in range(NUM_EPOCH):
+    running_loss = 0.0
+    for i, data in enumerate(train_dl):
+        inputs, labels = data
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % PRINT_EVERY == 0:
+            print('[%d, %d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / PRINT_EVERY))
+            running_loss = 0.0
+
+print('Finished Training')
