8c07488f7aadb27e8c3510a96793a1ad57829219
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-05-07 10:40:49 +0800

    update rnn

--- main_prob2.py
+++ main_prob2.py
@@ -50,15 +50,14 @@ loss_fn = nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters())
 
 step = 0
-for epoch in range(args.epoch):
-    logger.info('Train on epoch {}.'.format(epoch))
-    model.train()
-    train_iter = dataset.batch_iter(bs=BATCH_SIZE, len_limit=80)
+for epoch in range(NUM_EPOCH):
+
+    lstm_model.train()
+    train_iter = train_dl.batch_iter(bs=BATCH_SIZE, len_limit=80)
     for data in train_iter:
-        model.zero_grad()
+        lstm_model.zero_grad()
 
         sentence_in = data[args.domain]
-        # pad_tensor = torch.ones(BATCH_SIZE, 1, dtype=torch.long)
         pad_tensor = torch.ones(BATCH_SIZE, 1)
         pad_tensor.new_full((BATCH_SIZE, 1), dataset.get_id('<pad>'))
 
@@ -66,23 +65,20 @@ for epoch in range(args.epoch):
             pad_tensor = pad_tensor.cuda()
         targets = torch.cat([data[args.domain][:, 1:], pad_tensor], dim=1)
 
-        # pdb.set_trace()
-        pred_scores, _ = model(sentence_in.type(torch.LongTensor),
+        pred_scores, _ = lstm_model(sentence_in.type(torch.LongTensor),
                                data[args.domain + '_len'])
 
-        loss = loss_function(pred_scores.permute(0, 2, 1),
+        loss = loss_fn(pred_scores.permute(0, 2, 1),
                              targets.type(torch.LongTensor))
         loss.backward()
         optimizer.step()
 
-        if step % 200 == 0:
-            writer.add_scalar(args.log_path + 'loss', loss.data.item(), step)
+        if step % PRINT_EVERY == 0:
+            writer.add_scalar('train_loss', loss.data.item(), step)
 
-        if step % 2000 == 0:
-            logger.info('Loss = {}'.format(loss.data.item()))
+        if step % PRINT_EVERY == 0:
+            print('Loss = {}'.format(loss.data.item()))
         step += 1
-    if (epoch + 1) % args.lr_dec_ep == 0:
-        scheduler.step()
 
-writer.export_scalars_to_json(args.log_path + 'scalars.json')
-torch.save(model.state_dict(), args.save_path)
+
+writer.export_scalars_to_json(LOG_PATH + 'scalars.json')
--- rnn_model.py
+++ rnn_model.py
@@ -4,19 +4,45 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 class LSTM(nn.Module):
-
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+    def __init__(self,
+                 vocab_size,
+                 embedding_dim,
+                 hidden_size,
+                 output_size,
+                #  use_gpu=False,
+                 ckpt=None):
         super(LSTM, self).__init__()
-        self.hidden_dim = hidden_dim
+        self.emb = nn.Embedding(vocab_size, embedding_dim)
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        # self.out_pred = nn.Linear(hidden_size, output_size)
+        self.rnn = nn.GRU(
+            embedding_dim,
+            self.hidden_size,
+            batch_first=True,
+            bidirectional=False)
+        self.seq2pred = nn.Linear(self.hidden_size, self.output_size)
 
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
-        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+        self._ckpt = ckpt
         return
 
-    def forward(self, sentence):
-        embeds = self.word_embeddings(sentence)
-        lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
-        tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
-        tag_scores = F.log_softmax(tag_space, dim=1)
-        return tag_scores
+    def forward(self, src, src_lengths):
+        # print('->', src.device)
+        src_emb = self.emb(src)
+        packed_src = nn.utils.rnn.pack_padded_sequence(
+            src_emb, src_lengths.cpu().numpy(), batch_first=True)
+        packed_output, hidden = self.rnn(packed_src)
+        output, _ = nn.utils.rnn.pad_packed_sequence(
+            packed_output, batch_first=True)
+        pred = self.seq2pred(output)
+        # pdb.set_trace()
+        pred_scores = F.log_softmax(pred, dim=2)
+
+        # fwd_final = hidden[0:hidden.size(0):2]
+        # bwd_final = hidden[1:hidden.size(0):2]
+        # final = torch.cat([fwd_final, bwd_final], dim=2)
+        return pred_scores, hidden
+
+    def restore(self):
+        self.load_state_dict(torch.load(self._ckpt))
+        return
