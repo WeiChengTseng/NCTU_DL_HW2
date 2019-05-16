8abaaaa208e0050f0b712bd4c378764a2f4c06b4
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-05-16 18:00:41 +0800

    update

--- main_prob2_1.py
+++ main_prob2_1.py
@@ -13,7 +13,7 @@ import argparse
 import os
 import logging
 
-from rnn_model import RNN
+from rnn_model import LSTM
 from seq_preprocessing import SeqDataLoader, token_generation
 
 try:
@@ -31,16 +31,28 @@ def calc_accuracy(pred_scores, Y):
 
 ACCEPT = 'iclr/ICLR_accepted.xlsx'
 REJECT = 'iclr/ICLR_rejected.xlsx'
-NUM_EPOCH = 100
-BATCH_SIZE = 50
+MAX_LEN = 10
+
+NUM_EPOCH = 35
+BATCH_SIZE = 5
+
 USE_CUDA = True
-PRINT_EVERY = 10
+PRINT_EVERY = 20
+
 EMBEDDING_DIM = 10
 HIDDEN_DIM = 10
+LR_DECAY_RATE = 1
+OPTIMIZER = 'sgd_moment'
+
 DEVICE = torch.device("cuda") if (torch.cuda.is_available()
                                   and USE_CUDA) else torch.device("cpu")
-LOG_PATH = 'result/logs/rnn'
 
+NAME = 'lstm_bs{}_hidden{}_embed{}_lrdc{}_{}'.format(BATCH_SIZE, HIDDEN_DIM,
+                                                  EMBEDDING_DIM, LR_DECAY_RATE,
+                                                  OPTIMIZER)
+LOG_PATH = 'result/logs/lstm_/' + NAME
+SAVE_PATH = 'result/ckpt/lstm_/' + NAME + '.pth'
+CKPT_FILE = None
 accepted = pd.read_excel(ACCEPT, index_col=0)
 rejected = pd.read_excel(REJECT, index_col=0)
 
@@ -52,14 +64,30 @@ test_df = accepted[:50].append(rejected[:50])
 
 token_info = token_generation(accepted.append(rejected), True)
 
-train_dl = SeqDataLoader(train_df, token_info, DEVICE)
-test_dl = SeqDataLoader(test_df, token_info, DEVICE)
+train_dl = SeqDataLoader(train_df, token_info, max_len=MAX_LEN, device=DEVICE)
+test_dl = SeqDataLoader(test_df, token_info, max_len=MAX_LEN, device=DEVICE)
+pad_idx = token_info['token_map']['<pad>']
+lstm_model = LSTM(train_dl.n_token, EMBEDDING_DIM, HIDDEN_DIM, 2,
+                  pad_idx).to(DEVICE)
 
-lstm_model = RNN(train_dl.n_token, EMBEDDING_DIM, HIDDEN_DIM, 2).to(DEVICE)
-
-writer = SummaryWriter(LOG_PATH)
+writer_train = SummaryWriter(LOG_PATH + '/train')
+writer_test = SummaryWriter(LOG_PATH + '/test')
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(lstm_model.parameters())
+
+optims = {
+    'adam': torch.optim.Adam(lstm_model.parameters()),
+    'rmsprop': torch.optim.RMSprop(lstm_model.parameters()),
+    'sgd': torch.optim.SGD(lstm_model.parameters(), 0.1),
+    'sgd_moment': torch.optim.SGD(lstm_model.parameters(), 0.1, momentum=0.9)
+}
+optimizer = optims[OPTIMIZER]
+scheduler = optim.lr_scheduler.ExponentialLR(optimizer, LR_DECAY_RATE)
+
+if CKPT_FILE:
+    print('Load checkpoint!!')
+    checkpoint = torch.load(CKPT_FILE)
+    lstm_model.load_state_dict(checkpoint['model'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
 
 step = 0
 for epoch in range(NUM_EPOCH):
@@ -74,23 +102,32 @@ for epoch in range(NUM_EPOCH):
         optimizer.step()
 
         if step % PRINT_EVERY == 0:
-            writer.add_scalar('train_loss', loss.data.item(), step)
-            writer.add_scalar('train_acc', calc_accuracy(pred_scores, labels),
-                              step)
+            writer_train.add_scalar('loss', loss.data.item(), step)
+            writer_train.add_scalar('accuracy',
+                                    calc_accuracy(pred_scores, labels), step)
             print('Train Loss = {}'.format(loss.data.item()))
 
             with torch.no_grad():
                 acc_t, loss_list = [], []
-                test_iter = test_dl.batch_iter(bs=BATCH_SIZE)
+                test_iter = test_dl.batch_iter(bs=len(test_dl))
                 for seq_t, seq_len_t, labels_t in test_iter:
                     pred_scores_t = lstm_model(seq_t, seq_len_t)
                     loss_t = loss_fn(pred_scores_t, labels_t)
                     acc_t.append(calc_accuracy(pred_scores_t, labels_t))
                     loss_list.append(loss_t.data.item())
-                writer.add_scalar('test_loss', np.mean(loss_list), step)
-                writer.add_scalar('test_acc', np.mean(acc_t), step)
+                writer_test.add_scalar('loss', np.mean(loss_list), step)
+                writer_test.add_scalar('accuracy', np.mean(acc_t), step)
                 print('Test Loss = {}'.format(np.mean(loss_list)))
 
         step += 1
 
-writer.export_scalars_to_json(LOG_PATH + 'scalars.json')
+    if epoch % 10 == 0:
+        torch.save(
+            {
+                'model': lstm_model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'BATCH_SIZE': BATCH_SIZE,
+                'EPOCH': epoch,
+                'STEP': step
+            }, SAVE_PATH)
+    scheduler.step()
--- report_rnn.py
+++ report_rnn.py
@@ -1,12 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy import signal
 import csv
+plt.style.use('ggplot')
+
+
+def smoothing(curve):
+    return signal.savgol_filter(curve, 51, 3)
+
 
 f = csv.DictReader(
     open(
         './result/csv/run-lstm_bs5_hidden10_embed10_lrdc1_test-tag-accuracy.csv',
         'r'))
-
-for row in f:
-    print(row)
+step, value = zip(*[(int(r['Step']), 1 - float(r['Value'])) for r in f])
+plt.plot(step, smoothing(value), label='test')
+f = csv.DictReader(
+    open(
+        './result/csv/run-lstm_bs5_hidden10_embed10_lrdc1_train-tag-accuracy_train.csv',
+        'r'))
+step, value = zip(*[(int(r['Step']), 1 - float(r['Value'])) for r in f])
+plt.plot(step, smoothing(value), label='train')
+plt.xlabel('training step')
+plt.ylabel('accuracy')
+plt.title('LSTM Error Rate Curve')
+plt.legend()
+plt.show()
