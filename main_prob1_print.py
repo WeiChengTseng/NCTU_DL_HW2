66949e78c94c9a18cb62c6db3cc62f0831ef228a
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-05-19 15:30:11 +0800

    cm

--- main_prob1_print.py
+++ main_prob1_print.py
@@ -12,6 +12,60 @@ import argparse
 import os
 
 from cnn_model import CNN, DenseNet, SmallCNN, ResNet
+from sklearn.metrics import confusion_matrix
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
 
 try:
     os.environ["CUDA_VISIBLE_DEVICES"] = "1"
@@ -52,6 +106,8 @@ print_ds = torchvision.datasets.ImageFolder(root='./print/',
                                           transform=data_transform_test)
 ori_ds = torchvision.datasets.ImageFolder(root='./print/',
                                           transform=trans_ori)
+
+print(val_ds.classes)
 
 val_dl = torch.utils.data.DataLoader(val_ds,
                                      batch_size=BATCH_SIZE,
@@ -95,6 +151,12 @@ with torch.no_grad():
         label_all.append(labels)
     output_all = torch.cat(output_all)
     label_all = torch.cat(label_all)
+    _, pred_all = torch.max(output_all, 1)
+    confusion_mat = confusion_matrix(label_all.cpu.numpy(), pred_all.cpu().numpy())
+    np.set_printoptions(precision=3)
+    plot_confusion_matrix(label_all, pred_all, classes=val_ds.classes, normalize=True,
+                      title='Confusion Matrix')
+    plt.savefig('cm.png', dpi=400)
     for i in range(10):
         mask = label_all == i
         acc = calc_accuracy(output_all[mask], label_all[mask].cuda())
