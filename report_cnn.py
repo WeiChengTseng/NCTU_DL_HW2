f3495577eeb4d52d858df3a94eec26bec4db02cb
Wei-Cheng Tseng <33885553+WeiChengTseng@users.noreply.github.com>
2019-05-19 12:35:30 +0800

    update fig

--- report_cnn.py
+++ report_cnn.py
@@ -5,31 +5,88 @@ from scipy import signal
 import csv
 plt.style.use('ggplot')
 
+EPOCH = 200
 
-def smoothing(curve, a=51):
+def smoothing(curve, a=21):
     x = lambda a: max(a, 0)
-    return np.array(list(map(x, signal.savgol_filter(curve, a, 3))))
+    return np.array(list(map(x, signal.savgol_filter(curve, a, 2))))
 
+# -----------------------------------------------------------------------------
 
 plt.rcParams["figure.figsize"] = (8, 4.5)
 f = csv.DictReader(
-    open('./result/csv/run-lstm_bs5_hidden10_embed10_lrdc1_test-tag-loss.csv',
+    open('./result/csv/run-CNN_exp_kernel3_stride1_test-tag-accuracy.csv',
          'r'))
-step, value = zip(*[(int(r['Wall time']), 1-float(r['Value'])) for r in f])
+step, value = zip(*[(float(r['Wall time']), 1-float(r['Value'])) for r in f])
 step = np.array(step) - step[0]
-plt.plot(step, (value), label='test')
+step, value = step[:EPOCH], value[:EPOCH]
+plt.plot(step, smoothing(value), label='filter size 3')
 
 f = csv.DictReader(
-    open('./result/csv/run-lstm_bs5_hidden10_embed10_lrdc1_train-tag-loss.csv',
+    open('./result/csv/run-CNN_exp_kernel5_stride1_test-tag-accuracy.csv',
          'r'))
-step, value = zip(*[(int(r['Wall time']), 1-float(r['Value'])) for r in f])
+step, value = zip(*[(float(r['Wall time']), 1-float(r['Value'])) for r in f])
 step = np.array(step) - step[0]
-plt.plot(step, (value), label='train')
+step, value = step[:EPOCH], value[:EPOCH]
+plt.plot(step, smoothing(value), label='filter size 5')
 
-plt.xlabel('training time')
-plt.ylabel('Error Rate')
-plt.title('CNN with Different Filter size')
+f = csv.DictReader(
+    open('./result/csv/run-CNN_exp_kernel7_stride1_test-tag-accuracy.csv',
+         'r'))
+step, value = zip(*[(float(r['Wall time']), 1-float(r['Value'])) for r in f])
+step = np.array(step) - step[0]
+step, value = step[:EPOCH], value[:EPOCH]
+plt.plot(step, smoothing(value), label='filter size 7')
+
+f = csv.DictReader(
+    open('./result/csv/run-SmallCNN_crop8_wd0.0001_test-tag-accuracy.csv',
+         'r'))
+step, value = zip(*[(float(r['Wall time']), 1-float(r['Value'])) for r in f])
+step = np.array(step) - step[0]
+step, value = step[:EPOCH], value[:EPOCH]
+plt.plot(step, smoothing(value), label='filter size 3, 4 CNN')
+
+plt.yscale('log')
+plt.xlabel('training time (seconds)')
+plt.ylabel('error rate')
+plt.title('CNN with Different Filter Size')
 plt.legend()
 
 plt.savefig('cnn_filter.png', dpi=400)
+plt.close()
+
+# -----------------------------------------------------------------------------
+
+plt.rcParams["figure.figsize"] = (8, 4.5)
+f = csv.DictReader(
+    open('./result/csv/run-CNN_exp_kernel3_stride1_test-tag-accuracy.csv',
+         'r'))
+step, value = zip(*[(float(r['Wall time']), 1-float(r['Value'])) for r in f])
+step = np.array(step) - step[0]
+step, value = step[:EPOCH], value[:EPOCH]
+plt.plot(step, smoothing(value), label='stride 1')
+
+f = csv.DictReader(
+    open('./result/csv/run-CNN_exp_kernel3_stride2_test-tag-accuracy.csv',
+         'r'))
+step, value = zip(*[(float(r['Wall time']), 1-float(r['Value'])) for r in f])
+step = np.array(step) - step[0]
+step, value = step[:EPOCH], value[:EPOCH]
+plt.plot(step, smoothing(value), label='stride 2')
+
+f = csv.DictReader(
+    open('./result/csv/run-CNN_exp_kernel3_stride3_test-tag-accuracy.csv',
+         'r'))
+step, value = zip(*[(float(r['Wall time']), 1-float(r['Value'])) for r in f])
+step = np.array(step) - step[0]
+step, value = step[:EPOCH], value[:EPOCH]
+plt.plot(step, smoothing(value), label='stride 3')
+
+# plt.yscale('log')
+plt.xlabel('training time (seconds)')
+plt.ylabel('error rate')
+plt.title('CNN with Different Stride')
+plt.legend()
+
+plt.savefig('cnn_stride.png', dpi=400)
 plt.close()
